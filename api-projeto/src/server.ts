99e5746e5cb14479f6ed3647d74309bed04c7534
Gonçalo Rosa <goncalojmrosa@gmail.com>
2024-05-28 21:03:31 +0100

    Fix distance function

--- api-projeto/src/server.ts
+++ api-projeto/src/server.ts
@@ -117,37 +117,44 @@ app.post(
         createdAt: serverTimestamp(),
       });
 
-      const newDocument = (await getDoc(doc(collection(db, "crosswalks"), id))).data();
+      const newDocument = (
+        await getDoc(doc(collection(db, "crosswalks"), id))
+      ).data();
 
-      return reply.status(201).send(newDocument);
+      return reply.status(201);
     }
-
   }
 );
 
-
 // Function to calculate distance between two coordinates
-function calculateDistance({ lat1, lon1, lat2, lon2 }: { lat1: number, lon1: number, lat2: number, lon2: number })
-{
-  var R = 6371; // km
-  var dLat = toRad(lat2-lat1);
-  var dLon = toRad(lon2-lon1);
+function calculateDistance({
+  lat1,
+  lon1,
+  lat2,
+  lon2,
+}: {
+  lat1: number;
+  lon1: number;
+  lat2: number;
+  lon2: number;
+}) {
+  var R = 6371000;
+  var dLat = toRad(lat2 - lat1);
+  var dLon = toRad(lon2 - lon1);
   var lat1 = toRad(lat1);
   var lat2 = toRad(lat2);
 
-  var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
-    Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2);
-  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+  var a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2);
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
   var d = R * c;
   return d;
 }
 
-function toRad(value: number)
-{
-    return value * Math.PI / 180;
+function toRad(value: number) {
+  return (value * Math.PI) / 180;
 }
-
-
 
 app.delete("/crosswalks/:id", async (request, reply) => {
   try {
